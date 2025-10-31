224673b8c11d25e03bf39d48e83418b339c225ad
cesaltinofelix <cesaltinofelix2000@gmail.com>
2025-10-31 14:44:14 +0100

    feat: update About and Contact pages with new layout, images, and styles

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,11 @@
 import Button from "../components/button/Button"
 import styles from "./page.module.css"
-import about from "./../../../public/about.png"
+import Image from "next/image";
 const About = ()=>{
     return (
         <div className={styles.container}>
              <div className={styles.imgContainer}>
-                {/* <Image src={about} alt="About Image" fill={true}/> */}
+                <Image src="/about.png" className={styles.img} alt="About Image" fill={true}/>
              <div className={styles.imgText}>
                 <h1 className={styles.title}>Digital Storytellers</h1>
                 <h2 className={styles.desc}>Handcrafting award winning digital experiences</h2>
@@ -17,6 +17,12 @@ const About = ()=>{
                 <div className={styles.item}>
                     <h1 className={styles.title}>Who Are We?</h1>
                     <p className={styles.desc}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Donec vel mauris quam. Sed vel lacus id magna consequat 
+                        tincidunt. Nullam euismod, nisi vel consectetur interdum, 
+                        nisl nisi aliquet nunc, eget aliquam nisl nunc euismod nunc.
+                    </p>
+                     <p className={styles.desc}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                         Donec vel mauris quam. Sed vel lacus id magna consequat 
                         tincidunt. Nullam euismod, nisi vel consectetur interdum, 
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,23 @@
+import Button from "../components/button/Button"
 import styles from "./page.module.css"
-
+import Image from "next/image"
 const Contact = ()=>{
     return (
-        <div className={styles.container}>Contact</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Let's Get In Touch</h1>
+            <div className={styles.content}>
+                <div className={styles.imgContainer}>
+                    <Image src="/contact.png" className={styles.img} alt="Contact Image" fill={true}/>
+                </div>
+                <div className={styles.form}>
+                    <input type="text" placeholder="Your Name" className={styles.input} />
+                    <input type="email" placeholder="Your Email" className={styles.input} />
+                    <textarea placeholder="Your Message" cols={30} rows={10} className={styles.textarea}></textarea>
+                    <Button label="Send Message" url="#" />
+                </div>
+
+            </div>
+        </div>
     )
 }
 
