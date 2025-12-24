17680b1376e9e22620e410c3ac2e67c98e366653
cesaltinofelix <cesaltinofelix2000@gmail.com>
2025-12-24 17:40:19 +0100

    feat: create Portfolio layout and enhance styling for gallery items

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,10 +1,47 @@
-import styles from "./page.module.css"
+import Link from "next/link";
+import styles from "./page.module.css";
+import Image from "next/image";
 
-const Portfolio = ()=>{
-    return (
-        <div className={styles.container}>Portfolio</div>
-    )
-}
+const Portfolio = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <Image
+            className={styles.img}
+            src="/illustration.png"
+            alt="Illustrations"
+            layout="fill"
+            objectFit="cover"
+          />
 
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <Image
+            className={styles.img}
+            src="/websites.jpg"
+            alt="Websites"
+            layout="fill"
+            objectFit="cover"
+          />
 
-export default Portfolio
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href="/portfolio/applications" className={styles.item}>
+          <Image
+            className={styles.img}
+            src="/apps.jpg"
+            alt="Applications"
+            layout="fill"
+            objectFit="cover"
+          />
+          <span className={styles.title}>Applications</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
