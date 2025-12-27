c60d8c65b2f2cc8c89a68a6e1de7cbf0365a6a61
cesaltinofelix <cesaltinofelix2000@gmail.com>
2025-12-27 13:46:14 +0100

    feat: add SWR for data fetching in Dashboard, update Blog component to dynamically render posts, and include new dependencies in package.json

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,69 +2,39 @@ import Link from "next/link";
 import styles from "./page.module.css";
 import Image from "next/image";
 
-const Blog = () => {
+const Blog = async () => {
+  async function fetchPosts() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      cache: "force-cache",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch posts");
+    }
+    const data = await res.json();
+    return data;
+  }
+
+  const posts = await fetchPosts();
+
   return (
     <div className={styles.mainContainer}>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            className={styles.image}
-            src="https://images.pexels.com/photos/28839480/pexels-photo-28839480.jpeg"
-            alt=""
-            width={400}
-            height={250}
-          />
-        </div>
-        <div className={styles.content}>
-            <h1 className={styles.title}>Title</h1>
-            <p className={styles.desc}>Description</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            className={styles.image}
-            src="https://images.pexels.com/photos/28839480/pexels-photo-28839480.jpeg"
-            alt=""
-            width={400}
-            height={250}
-          />
-        </div>
-        <div className={styles.content}>
-            <h1 className={styles.title}>Title</h1>
-            <p className={styles.desc}>Description</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            className={styles.image}
-            src="https://images.pexels.com/photos/28839480/pexels-photo-28839480.jpeg"
-            alt=""
-            width={400}
-            height={250}
-          />
-        </div>
-        <div className={styles.content}>
-            <h1 className={styles.title}>Title</h1>
-            <p className={styles.desc}>Description</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            className={styles.image}
-            src="https://images.pexels.com/photos/28839480/pexels-photo-28839480.jpeg"
-            alt=""
-            width={400}
-            height={250}
-          />
-        </div>
-        <div className={styles.content}>
-            <h1 className={styles.title}>Title</h1>
-            <p className={styles.desc}>Description</p>
-        </div>
-      </Link>
+      {posts.map((post) => (
+        <Link href={`/blog/${post.id}`} className={styles.container} key={post.id}>
+          <div className={styles.imageContainer}>
+            <Image
+              className={styles.image}
+              src="https://images.pexels.com/photos/28839480/pexels-photo-28839480.jpeg"
+              alt=""
+              width={400}
+              height={250}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{post.title}</h1>
+            <p className={styles.desc}>{post.body}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,18 @@
-const Dashboard = ()=>{
-    return (
-        <div>Dashboard</div>
-    )
-}
+"use client";
+import React from "react";
+import useSWR from "swr";
 
+const Dashboard = () => {
+    const fetcher = (url) => fetch(url).then((r) => r.json());
+  const { data, error, isLoading } = useSWR(
+    "https:jsonhttps://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
 
-export default Dashboard
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
