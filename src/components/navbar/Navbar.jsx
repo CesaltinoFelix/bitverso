5cc1979abe66045a002a017742a71b97181e5a31
cesaltinofelix <cesaltinofelix2000@gmail.com>
2026-01-01 13:26:30 +0100

    fix: correct import path for DarkModeToggle component in Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import React from "react";
 import styles from "./page.module.css";
-import DarkModeToggle from "../darkModeToggle/DarkModeToggle";
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 import { signOut, useSession } from "next-auth/react";
 
 const links = [
